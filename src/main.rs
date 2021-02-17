978124e6b044e24e16ce260d0bc92c75fff9e6f3
Sycration <sycration@gmail.com>
2021-02-16 16:49:30 -0800

    Put basic Iced setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{Column, Element, Sandbox, Settings};
+
+pub fn main() -> iced::Result {
+    App::run(Settings::default())
+}
+
+#[derive(Default)]
+struct App {
+
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+}
+
+impl Sandbox for App {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn title(&self) -> String {
+        String::from("File Browser")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+           
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        Column::new()
+            .into()
+    }
 }
